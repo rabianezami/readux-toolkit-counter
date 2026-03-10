8f7b164445b157e43fe790630eefca6a21362bfd
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-11 00:35:38 +0430

    Add padding to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card({ children }) {
     return (
         <div className="
-           rounded py-5
+           rounded p-5
            border border-gray-100
            shadow bg-gray-700
            max-w-md mx-auto  
