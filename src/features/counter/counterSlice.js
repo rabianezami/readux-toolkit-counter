4d4bbf229a956c5bfa2a8c840b4b7acd1e620d35
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-10 10:31:41 +0430

    feat: add counterSlice

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+initialState =
+    { value: 0 }
+
+const counterSlice = createSlice({
+    name: "counter",
+    initialState,
+    reducers: {
+        increment: (state) => { state.value += 1 },
+
+        decrement: (state) => { state.value -= 1 },
+
+        reset: (state) => { state.value = 0 },
+
+        incrementByAmount: (state, action) => { state.value += action.payload }
+    }
+})
+
+export const { increment, decrement, reset, incrementByAmount } = counterSlice.actions
+
+export default counterSlice.reducer
