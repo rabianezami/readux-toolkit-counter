f8233f0da5a65531dc3832ee7f73b52149861914
Rabia Zia Nezami <rabianezami786@gmail.com>
2026-03-11 00:35:10 +0430

    refactor: compose all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+  reset
+} from "./features/counter/counterSlice"
+
+import Card from "./components/Card"
+import CounterValue from "./components/CounterValue"
+import CounterControls from "./components/CounterControls"
+import AmountAdder from "./components/AmountAdder"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+
+  const [amount, setAmount] = useState("")
+
+  const parsedAmount = Number(amount)
+  const amountIsValid = Number.isFinite(parsedAmount)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="
+       min-h-screen 
+       p-15 
+       bg-gray-800
+       text-white
+     "
+    >
+
+      <Card>
+        <h1 className="
+           text-md font-semibold 
+           mb-2 opacity-70
+         "
+        >
+          Redux Toolkit Counter
+        </h1>
+
+        <p className="opacity-80 mt-5 lineHeight-1">
+          This project demonstrates RTK fundamentals: <b>store</b>, <b>slice</b>,
+          <b>reducers</b>, and React bindings via <b>useSelector</b> and{" "}
+          <b>useDispatch</b>.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+        <div className="
+          grid grid-cols-[1fr_auto] 
+          gap-2 alignItems-center 
+          mt-5
+         "
+        >
+          <CounterValue value={count}/>
+
+          <CounterControls
+            onIncrement={() => dispatch(increment())}
+            onDecrement={() => dispatch(decrement())}
+            onReset={() => dispatch(reset())}
+          />
+        </div>
+
+        <AmountAdder
+          amount={amount}
+          setAmount={setAmount}
+          amountIsValid={amountIsValid}
+          onAdd={() => dispatch(incrementByAmount(parsedAmount))}
+        />
+
+      </Card>
+    </div>
   )
 }
 
